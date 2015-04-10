4001db5cd884228727659775a48974e0cc24445e
Aristeides Stathopoulos <aristath@gmail.com>
2015-04-10 05:30:02 +0300

    add off-canvas sidebar

--- includes/class-maera-md-timber.php
+++ includes/class-maera-md-timber.php
@@ -11,12 +11,13 @@ class Maera_MD_Timber extends Maera_Material {
 	 */
 	function timber_global_context( $data ) {
 
-		$data['header_img']         = Maera_Material::custom_header_url();
-		$data['sidebar']['header']  = Timber::get_widgets( 'sidebar_header' );
-		$data['sidebar']['footer']  = Timber::get_widgets( 'sidebar_footer' );
-		$data['layout']             = Maera()->shell->instance->layout;
-		$data['menu']['offcanvas']  = has_nav_menu( 'offcanvas' )  ? new TimberMenu( 'offcanvas' )  : null;
-		$data['menu']['horizontal'] = has_nav_menu( 'horizontal' ) ? new TimberMenu( 'horizontal' ) : null;
+		$data['header_img']           = Maera_Material::custom_header_url();
+		$data['sidebar']['header']    = Timber::get_widgets( 'sidebar_header' );
+		$data['sidebar']['footer']    = Timber::get_widgets( 'sidebar_footer' );
+		$data['sidebar']['offcanvas'] = Timber::get_widgets( 'offcanvas' );
+		$data['layout']               = Maera()->shell->instance->layout;
+		$data['menu']['offcanvas']    = has_nav_menu( 'offcanvas' )  ? new TimberMenu( 'offcanvas' )  : null;
+		$data['menu']['horizontal']   = has_nav_menu( 'horizontal' ) ? new TimberMenu( 'horizontal' ) : null;
 
 		return $data;
 
